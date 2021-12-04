1a738c4be85a8607cd7f2ec4bb95fa3095d7f550
Oolaa Pleur <oolaa.pleur@gmail.com>
2021-12-04 17:42:10 +0200

    added splash screen and added map for tables(for 2022 table mostly)

--- lib/screens/show_hourly_pay_month_table_screen.dart
+++ lib/screens/show_hourly_pay_month_table_screen.dart
@@ -8,7 +8,9 @@ class ShowHourlyPayMonthTableScreen extends StatefulWidget {
 }
 
 class _ShowHourlyPayMonthTableScreenState extends State<ShowHourlyPayMonthTableScreen> {
-  static String imageUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQtiXSYwaCWDjeBh2hqCIqjjVECKptGphKrZ6UoDPXBJ6zgpo_Lex2E715W7IaIRezV5W3I8J0Xe__f/pubchart?oid=31468050&format=image';
+  Map<String, String> monthTables = {'11': '31468050', '12': '240229873'}; // map of months number as key and table number as value
+  late String tableNumber = monthTables[DateTime.now().month.toString()].toString(); // get table number for current month
+  late String tableUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQtiXSYwaCWDjeBh2hqCIqjjVECKptGphKrZ6UoDPXBJ6zgpo_Lex2E715W7IaIRezV5W3I8J0Xe__f/pubchart?oid=$tableNumber&format=image&';
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -17,13 +19,13 @@ class _ShowHourlyPayMonthTableScreenState extends State<ShowHourlyPayMonthTableS
         height: MediaQuery.of(context).size.height,
         child: InkWell(
             onTap: () {setState(() {
-              imageUrl='https://docs.google.com/spreadsheets/d/e/2PACX-1vQtiXSYwaCWDjeBh2hqCIqjjVECKptGphKrZ6UoDPXBJ6zgpo_Lex2E715W7IaIRezV5W3I8J0Xe__f/pubchart?oid=31468050&format=image&${DateTime.now().microsecondsSinceEpoch}';
+              tableUrl=tableUrl+DateTime.now().microsecondsSinceEpoch.toString();
             });},
             child:
             Transform.rotate(
               angle: 0,
               child: Image.network(
-                imageUrl, fit: BoxFit.cover,
+                tableUrl, fit: BoxFit.cover,
               ),
             )
         ),
--- lib/screens/show_table_screen.dart
+++ lib/screens/show_table_screen.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 
 class ShowTableScreen extends StatefulWidget {
   static const routeName = '/show-table-screen';
-  const ShowTableScreen({Key? key}) : super(key: key);
 
   @override
   _ShowTableScreenState createState() => _ShowTableScreenState();
 }
 
 class _ShowTableScreenState extends State<ShowTableScreen> {
-  String tableUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQtiXSYwaCWDjeBh2hqCIqjjVECKptGphKrZ6UoDPXBJ6zgpo_Lex2E715W7IaIRezV5W3I8J0Xe__f/pubchart?oid=2124533607&format=image&';
+  Map<String, String> monthTables = {'11': '2124533607', '12': '481373075'}; // map of months number as key and table number as value
+  late String tableNumber = monthTables[DateTime.now().month.toString()].toString(); // get table number for current month
+  late String tableUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQtiXSYwaCWDjeBh2hqCIqjjVECKptGphKrZ6UoDPXBJ6zgpo_Lex2E715W7IaIRezV5W3I8J0Xe__f/pubchart?oid=$tableNumber&format=image&';
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
